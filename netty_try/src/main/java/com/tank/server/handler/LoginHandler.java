b6583e05cc626b7c9796f925983a3d55912fe297
Tank <fuch@fullsample.com>
2019-01-14 13:35:19 +0800

    [fuchun] #Tech add login handler

--- netty_try/src/main/java/com/tank/server/handler/LoginHandler.java
+++ netty_try/src/main/java/com/tank/server/handler/LoginHandler.java
@@ -1,8 +1,14 @@
 package com.tank.server.handler;
 
+import com.google.common.base.Preconditions;
+import com.tank.common.BytesUtil;
+import com.tank.server.container.OnlineUsers;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Objects;
 
 /**
  * @author fuchun
@@ -18,7 +24,27 @@ public class LoginHandler extends ChannelInboundHandlerAdapter {
 
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    Objects.requireNonNull(msg);
     log.info("channel read");
+    if (msg instanceof ByteBuf) {
+      ByteBuf buffer = (ByteBuf) msg;
+      byte[] data = new byte[buffer.readableBytes()];
+      buffer.readBytes(data);
+      final String str = BytesUtil.toStr(data);
+      String[] result = str.split(":");
+      Objects.requireNonNull(result);
+      Preconditions.checkArgument(result.length == 2, "chat format not right");
+      String username = result[0];
+      String password = result[1];
+
+      if ("123".equalsIgnoreCase(password)) {
+        Objects.requireNonNull(this.onlineUsers);
+        this.onlineUsers.add(username, ctx);
+//        this.onlineUsers.broadcast("on line users is:" + this.onlineUsers.number());
+      }
+
+    }
+
 
   }
 
@@ -31,4 +57,6 @@ public class LoginHandler extends ChannelInboundHandlerAdapter {
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
     log.info("exception caught");
   }
+
+  private OnlineUsers onlineUsers = OnlineUsers.newInstance();
 }
